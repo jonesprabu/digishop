bcec00fa5c97e32ee105752754110f4ba99bd8d9
jonesprabu <jonesprabu@gmail.com>
2017-11-01 23:50:29 +0530

    remove server sdk

--- src/app/shared/sdk/models/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-export * from './User';
-export * from './Product';
-export * from './Order';
-export * from './Card';
-export * from './BaseModels';
-export * from './FireLoopRef';
