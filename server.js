c26f44264caee1818b826299df492d57655a31af
jonesprabu <jonesprabu@gmail.com>
2017-11-02 17:27:33 +0530

    heroku related changes

--- server.js
+++ server.js
@@ -2,16 +2,11 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-//const bodyParser = require('body-parser');
 
 // Get our API routes
 //const api = require('./server/routes/api');
 
 const app = express();
-
-// Parsers for POST data
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -38,4 +33,4 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+server.listen(port, () => console.log(`Digitalretail app running on localhost:${port}`));
